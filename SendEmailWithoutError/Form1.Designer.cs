b7e92524a91945785608a18687bd0407c6f4918c
Joechen9071 <nmb9071@gmail.com>
2017-11-11 02:08:22 -0600

    Initial commit

---
+++ SendEmailWithoutError/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace SendEmailWithoutError
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Body = new System.Windows.Forms.RichTextBox();
+            this.To = new System.Windows.Forms.TextBox();
+            this.Subject = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Pw = new System.Windows.Forms.TextBox();
+            this.Emaill = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.AttachFile = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Body
+            // 
+            this.Body.Location = new System.Drawing.Point(12, 227);
+            this.Body.Name = "Body";
+            this.Body.ReadOnly = true;
+            this.Body.Size = new System.Drawing.Size(360, 177);
+            this.Body.TabIndex = 0;
+            this.Body.Text = "";
+            // 
+            // To
+            // 
+            this.To.Location = new System.Drawing.Point(65, 175);
+            this.To.Name = "To";
+            this.To.ReadOnly = true;
+            this.To.Size = new System.Drawing.Size(194, 20);
+            this.To.TabIndex = 1;
+            // 
+            // Subject
+            // 
+            this.Subject.Location = new System.Drawing.Point(65, 201);
+            this.Subject.Name = "Subject";
+            this.Subject.ReadOnly = true;
+            this.Subject.Size = new System.Drawing.Size(194, 20);
+            this.Subject.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.Pw);
+            this.groupBox1.Controls.Add(this.Emaill);
+            this.groupBox1.Location = new System.Drawing.Point(12, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(360, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Login";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Note:Only Gmail can be used";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(214, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Confirmed";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Email:";
+            // 
+            // Pw
+            // 
+            this.Pw.Location = new System.Drawing.Point(65, 51);
+            this.Pw.Name = "Pw";
+            this.Pw.PasswordChar = '*';
+            this.Pw.Size = new System.Drawing.Size(129, 20);
+            this.Pw.TabIndex = 1;
+            // 
+            // Emaill
+            // 
+            this.Emaill.Location = new System.Drawing.Point(65, 25);
+            this.Emaill.Name = "Emaill";
+            this.Emaill.Size = new System.Drawing.Size(129, 20);
+            this.Emaill.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(265, 199);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "To:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 204);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Subject:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(265, 173);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(24, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // AttachFile
+            // 
+            this.AttachFile.Location = new System.Drawing.Point(113, 409);
+            this.AttachFile.Name = "AttachFile";
+            this.AttachFile.ReadOnly = true;
+            this.AttachFile.Size = new System.Drawing.Size(188, 20);
+            this.AttachFile.TabIndex = 8;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 409);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Add Attachment";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(307, 407);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(65, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Clear";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 409);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(95, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Add More";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Visible = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 452);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.AttachFile);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Subject);
+            this.Controls.Add(this.To);
+            this.Controls.Add(this.Body);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Send Your Email";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox Body;
+        private System.Windows.Forms.TextBox To;
+        private System.Windows.Forms.TextBox Subject;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Pw;
+        private System.Windows.Forms.TextBox Emaill;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox AttachFile;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ SendEmailWithoutError/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+using System.IO;
+
+namespace SendEmailWithoutError
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        NetworkCredential Nw;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string email = Emaill.Text;
+            string pw = Pw.Text;
+            bool exist = InputEmailIsCorrect(email);
+            if (!exist)
+            {
+                MessageBox.Show("Invalid Email Re-Enter Your Email");
+            }
+            else
+            {
+                LoginToGmail(email,pw);
+                EnableInput();
+            }
+
+        }
+        private bool InputEmailIsCorrect(string email)
+        {
+            try
+            {
+                if (Emaill.Text.Contains("@gmail.com") || Emaill.Text.Contains("@Gmail.com"))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            
+        }
+        private NetworkCredential LoginToGmail(string emaill,string pw)
+        {
+            Nw = new NetworkCredential(emaill,pw);
+            return Nw;
+        }
+        private void EnableInput()
+        {
+            To.ReadOnly = false;
+            Subject.ReadOnly = false;
+            Body.ReadOnly = false;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+           try
+           {
+                string[] email = To.Text.Split(',');
+                string[] attachments = AttachFile.Text.Split(',');
+                for (int i=0;i<email.Length;i++) {
+                    MailMessage msg = new MailMessage(Emaill.Text, email[i], Subject.Text, Body.Text);
+                    for (int x=0;x<attachments.Length;x++)
+                    {
+                        msg.Attachments.Add(new Attachment(attachments[x]));
+                    }
+                    SmtpClient sm = new SmtpClient("smtp.gmail.com");
+                    sm.UseDefaultCredentials = false;
+                    sm.Credentials = LoginToGmail(Emaill.Text, Pw.Text);
+                    sm.EnableSsl = true;
+                    sm.Port = 587;//Gmail Smtp port 465 or 587
+                    sm.Send(msg); }
+                MessageBox.Show("Message Sent !");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to send the Email, Please Check Following:"+"\n \n"+"1.Google Security Alert"+"\n"+"2.Gmail Password");
+            }
+           
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog of = new OpenFileDialog();
+                if (DialogResult.OK == of.ShowDialog()) {
+                    string path = of.FileName;
+                    string[] lines = System.IO.File.ReadAllLines(path);
+                    string email = lines[0];
+                    for (int i = 1; i < lines.Length; i++)
+                    {
+                        email = email + "," + lines[i];
+                    }
+                    To.Text = email; }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog Of = new OpenFileDialog();
+                if (DialogResult.OK == Of.ShowDialog() && button4.Text.Equals("Add Attachment"))
+                {
+                    AttachFile.Text = Of.FileName;
+                    button6.Visible = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            AttachFile.Text = "";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string CurrentSelectedFiles = AttachFile.Text;
+                OpenFileDialog Of = new OpenFileDialog();
+                if (DialogResult.OK == Of.ShowDialog())
+                {
+                    CurrentSelectedFiles = CurrentSelectedFiles + "," + Of.FileName;
+                    AttachFile.Text = CurrentSelectedFiles;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
